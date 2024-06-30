aed31275aed411730ee9b9173fba1c2d51e43630
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-06-30 23:45:29 +0530

    added echo function

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     
-    knownUserCommands = ["exit 0"]
+    knownUserCommands = ["exit", "echo"]
 
     # Wait for user input
     
@@ -13,14 +13,21 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
-        if command not in knownUserCommands: 
+        commandcheck  = command.split(" ")[1:]
+        thing = " ".join(commandcheck)
+        if command.split(" ")[0] not in knownUserCommands: 
             sys.stdout.write(f"{command}: command not found\n")
             iknow() 
-
+        elif command.split(" ")[0] == "echo": 
+            sys.stdout.write(f"{thing} \n")
+            iknow()
+        elif command.split(" ")[0] == "exit":
+            if commandcheck == ["0"] : 
+                sys.stdout.write("thanks for checking out")
+            else :
+                iknow()
+        else :
+            iknow()
     iknow()
-
-        
-    
-
 if __name__ == "__main__":
     main()

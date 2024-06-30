4a3891be77ef451ad2fc53bd20adc474c64b3e2d
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-07-01 00:29:34 +0530

    added path to files

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
-
+import os
 
 def main():
     # Uncomment this block to pass the first stage
     
     knownUserCommands = ["exit", "echo", "type"]
+    PATH = os.environ.get("PATH")
 
     # Wait for user input
     
@@ -24,15 +25,21 @@ def main():
         elif command.split(" ")[0] == "exit":
             if commandcheck != ["0"] : 
                 iknow()
-        elif command.split(" ")[0] == "type": 
-            if len(command.split(" ")) > 1: 
-                if command.split(" ")[1] not in knownUserCommands: 
-                    sys.stdout.write(f"{thing}: not found\n")
-                    iknow()
-                else : 
-                    sys.stdout.write(f"{thing} is a shell builtin\n")
-                    iknow()
+        elif command.split(" ")[0] == "type" and len(command.split(" ")) > 1: 
+            cmd = command.split(" ")[1]
+            cmd_path = None
+            paths = PATH.split(":")
+            for path in paths:
+                if os.path.isfile(f"{path}/{cmd}"):
+                    cmd_path = f"{path}/{cmd}"
+            
+            if cmd in knownUserCommands: 
+                sys.stdout.write(f"{thing} is a shell builtin\n")
+                iknow()
+            elif cmd_path:
+                sys.stdout.write(f"{cmd} is {cmd_path}\n")
             else : 
+                sys.stdout.write(f"{thing}: not found\n")
                 iknow()
         else :
             iknow()
